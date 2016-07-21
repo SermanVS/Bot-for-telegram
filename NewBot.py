2a56305f21d2b49202dd4820cb25f25bf2dbb60b
SermanVS <sergey.zr.co.uk@gmail.com>
2016-07-22 01:32:33 +0300

    Add files via upload
    This bot will send you photo with a file_id

---
+++ NewBot.py
@@ -0,0 +1,17 @@
+import telebot
+import os
+import time
+bot = telebot.TeleBot('208047337:AAG58sXqlAtPYxoh0zdVmMJVuibiPemvMos')
+
+@bot.message_handler(content_types = ["text"])
+def find_file_ids(message):
+    for file in os.listdir('botphotos/'):
+        if file.split('.')[-1] == 'jpg':
+            f = open('botphotos/'+file, 'rb')
+            res = bot.send_photo(message.chat.id, f, None)
+            print(res.photo[1])
+        time.sleep(3)
+        
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
